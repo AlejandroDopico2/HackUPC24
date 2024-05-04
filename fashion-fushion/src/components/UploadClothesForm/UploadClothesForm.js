0d9e75eb0995a8f2ddb75d6f2cab7d462da5140b
Mar Alonso <mar.alonso@udc.es>
2024-05-05 00:32:15 +0200

    Added images to Supabase

--- fashion-fushion/src/components/UploadClothesForm/UploadClothesForm.js
+++ fashion-fushion/src/components/UploadClothesForm/UploadClothesForm.js
@@ -1,11 +1,15 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { supabase } from "../../utils/supabase";
 
 const UploadClothesForm = () => {
   const [garmentType, setGarmentType] = useState('');
+  const [season, setSeason] = useState('');
   const [seasons, setSeasons] = useState('');
   const [colors, setColors] = useState('');
+  const [color, setColor] = useState('');
   const [garmentTypes, setGarmentTypes] = useState([]);
+  const [image, setImage] = useState(null);
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     fetch('http://127.0.0.1:8000/getGarmentTypes')
@@ -13,21 +17,60 @@ const UploadClothesForm = () => {
       .then(data => setGarmentTypes(data))
       .catch(error => console.error('There was an error!', error));
 
-
     fetch('http://127.0.0.1:8000/getSeasons')
       .then(response => response.json())
       .then(data => setSeasons(data))
       .catch(error => console.error('There was an error!', error));
 
-      fetch('http://127.0.0.1:8000/getColors')
+    fetch('http://127.0.0.1:8000/getColors')
       .then(response => response.json())
       .then(data => setColors(data))
       .catch(error => console.error('There was an error!', error));
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
   }, []);
 
-  const handleSubmit = (event) => {
+  const handleImageChange = async (e) => {
+   setImage(e.target.files[0]);
+   console.log(image);
+
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`Garment Type: ${garmentType}, Season: ${seasons}, Color: ${colors}`);
+
+
+    console.log(`Garment Type: ${garmentType}, Season: ${season}, Color: ${color}`);
+    console.log(`Image: `, image);
+
+  supabase.storage.from('images2').upload( image.name, image).then(({data, error}) => {
+    if (error) {
+        console.error('There was an error uploading the image:', error.message);
+        return;
+    }
+
+    supabase
+        .from('Garment')
+        .insert([
+            {
+                type: garmentType.valueOf(),
+                season: season.valueOf(),
+                color: color.valueOf(),
+                image: data.fullPath,
+                user_id: session.user.uuid
+            },
+        ])
+        .then(({data: insertData, error: insertError}) => {
+            if (insertError) {
+                console.error('There was an error saving the garment:', insertError.message);
+            } else {
+                console.log('Garment saved successfully:', insertData);
+            }
+        });
+});
+
   };
 
   return (
@@ -43,7 +86,7 @@ const UploadClothesForm = () => {
       </label>
       <label>
         Season:
-       <select value={seasons} onChange={e => setSeasons(e.target.value)}>
+       <select value={season} onChange={e => setSeason(e.target.value)}>
           <option value="">--Select a season--</option>
           {Object.entries(seasons).map(([key, value]) => (
             <option key={key} value={value}>{key}</option>
@@ -52,12 +95,16 @@ const UploadClothesForm = () => {
       </label>
       <label>
         Color:
-       <select value={colors} onChange={e => setColors(e.target.value)}>
+       <select value={color} onChange={e => setColor(e.target.value)}>
           <option value="">--Select a color--</option>
           {Object.entries(colors).map(([key, value]) => (
             <option key={key} value={value}>{key}</option>
           ))}
         </select>
+      </label>
+      <label>
+        Image:
+        <input type="file" onChange={handleImageChange} />
       </label>
       <input type="submit" value="Upload" />
     </form>
